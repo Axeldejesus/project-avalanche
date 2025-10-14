769b4bc9abc81c62e3d4b2e78cdf9425ef63e1c1
Axeldejesus <axelteixeiradejesus@gmail.com>
2025-10-14 09:23:48 +0200

    remove logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Navigation from './Navigation';
 import AuthButtonsWrapper from './AuthButtonsWrapper';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image src="/logo-avalanche.png" alt="Avalanche" width={100} height={30} />
-      </div>
+      <Link href="/" className={styles.logo}>
+        AVALANCHE
+      </Link>
       
       <Navigation />
       
